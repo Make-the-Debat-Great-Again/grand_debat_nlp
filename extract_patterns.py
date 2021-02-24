c1b9e5f063b0a0eb9fe52c1ab2eb755e1d2d5d17
Jacques <jacques.fize@insa-lyon.fr>
2021-02-24 12:14:04 +0100

    DEP PARSING ->Debug + use nlp.pipe+ add command line parameter+ now work with all grand débat dataset

--- extract_patterns.py
+++ extract_patterns.py
@@ -4,66 +4,65 @@ from spacy import displacy
 
 import pandas as pd
 from tqdm import tqdm
-from lib.constant import analysed_questions
+from lib.constant import analysed_questions,question_patterns
 
 from spacy.matcher import DependencyMatcher
 from pattern import enjeux
 import json
+import argparse
 
 index_pattern = {
-    1:"OBSERVATION",
-    2:"PROPOSITION"
+    1: "OBSERVATION",
+    2: "PROPOSITION",
+    3: "ITEMLIST",
+    4: "SITUATIONPERSONNELLE"
 }
 
-question_patterns = {
-    'QUXVlc3Rpb246MTYx - Que faudrait-il faire selon vous pour apporter des réponses à ce problème ?': [2],
-    'QUXVlc3Rpb246MTQ3 - Si oui, de quelle manière votre vie quotidienne est-elle touchée par le changement climatique ?': [1],
-    "QUXVlc3Rpb246MTUw - Qu'est-ce qui pourrait vous inciter à changer vos comportements comme par exemple mieux entretenir " \
-    + "et régler votre chauffage, modifier votre manière de conduire ou renoncer à prendre votre véhicule pour de très petites distances ?": [2],
-    'QUXVlc3Rpb246MTUx - Quelles seraient pour vous les solutions les plus simples et les plus supportables sur un plan financier pour vous inciter à changer vos comportements ?': [2],
-    'QUXVlc3Rpb246MTU1 - Si oui, que faudrait-il faire pour vous convaincre ou vous aider à utiliser ces solutions alternatives ?': [2],
-    "QUXVlc3Rpb246MjA3 - Si non, quelles sont les solutions de mobilité alternatives que vous souhaiteriez pouvoir utiliser ?": [2]
-    }
+parser = argparse.ArgumentParser()
+parser.add_argument("dataset_code",choices=list(question_patterns.keys()))
+parser.add_argument("dataset_fn")
+
+args= parser.parse_args()
+
 
 # INIT SPACY MODEL
 nlp = spacy.load("fr_core_news_lg")
 nlp.add_pipe("merge_noun_chunks")
 
 # DATA
-df_data = pd.read_csv("data/LA_TRANSITION_ECOLOGIQUE.csv", dtype={"authorZipCode": str}).fillna("")
-
+df_data = pd.read_csv(args.dataset_fn, dtype={"authorZipCode": str}).fillna("")
 
 # PREPARE MATCHER
 matcher = DependencyMatcher(nlp.vocab)
 prop = enjeux.Proposition()
 observ = enjeux.ObservationObjectif()
 
-for pat_key,pat_value in observ.get_patterns().items():
+for pat_key, pat_value in observ.get_patterns().items():
     matcher.add("OBSERVATION|{0}".format(pat_key), [pat_value])
-for pat_key,pat_value in prop.get_patterns().items():
+for pat_key, pat_value in prop.get_patterns().items():
     matcher.add("PROPOSITION|{0}".format(pat_key), [pat_value])
 
-for question,pattern_idx in question_patterns.items():
+for question, pattern_idx in question_patterns[args.dataset_code].items():
     results = []
     id_ = df_data["id"].values
     zipCode = df_data["authorZipCode"].values
     data_question = df_data[question].values
-    for ix, doc in tqdm(enumerate(data_question),total=len(data_question)):
+    for ix, doc in tqdm(enumerate(nlp.pipe(data_question)), total=len(data_question)):
         question_res = []
-        response = nlp(doc)
+        #response = nlp(doc)
+        response = doc
         matches = matcher(response)
-        if len(matches)>0:
+        if len(matches) > 0:
             for pat in pattern_idx:
                 found = []
                 if pat == 2:
                     found.extend(prop.parse_output(response,
-                                  [ma for ma in matches if nlp.vocab.strings[ma[0]].split("|")[0] == "PROPOSITION"]))
-                else:
+                                                   [ma for ma in matches if
+                                                    nlp.vocab.strings[ma[0]].split("|")[0] == "PROPOSITION"]))
+                elif pat == 1:
                     found.extend(observ.parse_output(response,
-                                    [ma for ma in matches if nlp.vocab.strings[ma[0]].split("|")[0] == "OBSERVATION"]))
+                                                     [ma for ma in matches if
+                                                      nlp.vocab.strings[ma[0]].split("|")[0] == "OBSERVATION"]))
                 question_res.append(found)
-            results.append([id_[ix],zipCode[ix],question_res])
-    json.dump(results,open(question.split(" - ")[0]+".json",'w'))
-
-
-
+            results.append([id_[ix], zipCode[ix], question_res])
+    json.dump(results, open(question.split(" - ")[0] + ".json", 'w'))
--- lib/constant.py
+++ lib/constant.py
@@ -112,3 +112,118 @@ analysed_questions  = {
     "QUXVlc3Rpb246MTY3 - Y a-t-il d'autres points sur les impôts et les dépenses sur lesquels vous souhaiteriez vous exprimer ?"
     ]
 }
+
+question_patterns = {
+    "transition_eco":{
+        "QUXVlc3Rpb246MTYw - Quel est aujourd'hui pour vous le problème concret le plus important dans le domaine de "
+        "l'environnement ?": [1],
+        'QUXVlc3Rpb246MTYx - Que faudrait-il faire selon vous pour apporter des réponses à ce problème ?': [2],
+        'QUXVlc3Rpb246MTQ3 - Si oui, de quelle manière votre vie quotidienne est-elle touchée par le changement climatique ?': [1],
+        "QUXVlc3Rpb246MTQ5 - Si oui, que faites-vous aujourd'hui pour protéger l'environnement et/ou que pourriez-vous faire ?": [2],
+        "QUXVlc3Rpb246MTUw - Qu'est-ce qui pourrait vous inciter à changer vos comportements comme par exemple mieux entretenir et" \
+        + "régler votre chauffage, modifier votre manière de conduire ou renoncer à prendre votre véhicule pour de très petites distances ?": [2],
+        "QUXVlc3Rpb246MTUx - Quelles seraient pour vous les solutions les plus simples et les plus supportables" \
+        + " sur un plan financier pour vous inciter à changer vos comportements ?": [2],
+        'QUXVlc3Rpb246MTUz - Si oui, que faudrait-il faire pour vous convaincre ou vous aider à changer de mode de chauffage ?': [2],
+        'QUXVlc3Rpb246MTU1 - Si oui, que faudrait-il faire pour vous convaincre ou vous aider à utiliser ces solutions alternatives ?': [2],
+        'QUXVlc3Rpb246MjA3 - Si non, quelles sont les solutions de mobilité alternatives que vous souhaiteriez pouvoir utiliser ?': [2],
+        'QUXVlc3Rpb246MTU3 - Et qui doit selon vous se charger de vous proposer ce type de solutions alternatives ?': [2],
+        "QUXVlc3Rpb246MTU4 - Que pourrait faire la France pour faire partager ses choix en matière d'environnement au niveau européen et international ?": [2],
+        "QUXVlc3Rpb246MTU5 - Y a-t-il d'autres points sur la transition écologique sur lesquels vous souhaiteriez vous exprimer ?": [1, 2]
+    },
+    "fiscalite_et_depense":{
+        "QUXVlc3Rpb246MTYy - Quelles sont toutes les choses qui pourraient être faites pour améliorer l'information des citoyens sur l'utilisation des impôts ?":[2],
+        "QUXVlc3Rpb246MTYz - Que faudrait-il faire pour rendre la fiscalité plus juste et plus efficace ?":[2],
+        "QUXVlc3Rpb246MTY0 - Quels sont selon vous les impôts qu'il faut baisser en priorité ?":[3],
+        "QUXVlc3Rpb246MjA2 - Afin de financer les dépenses sociales, faut-il selon vous...":[2],
+        "QUXVlc3Rpb246MjA1 - S'il faut selon vous revoir les conditions d'attribution de certaines aides sociales, lesquelles doivent être concernées ?":[1,3],
+        "QUXVlc3Rpb246MTY1 - Quels sont les domaines prioritaires où notre protection sociale doit être renforcée ?":[1,3],
+        "QUXVlc3Rpb246MTY2 - Pour quelle(s) politique(s) publique(s) ou pour quels domaines d'action publique, seriez-vous prêts à payer plus d'impôts ?":[1],
+        "QUXVlc3Rpb246MTY3 - Y a-t-il d'autres points sur les impôts et les dépenses sur lesquels vous souhaiteriez vous exprimer ?":[1,2]
+    }
+    ,"demo_et_citoy":{
+        "QUXVlc3Rpb246MTA3 - En qui faites-vous le plus confiance pour vous faire représenter dans la société et pourquoi ?":[1,3],
+        "QUXVlc3Rpb246MTA4 - En dehors des élus politiques, faut-il donner un rôle plus important aux associations et aux organisations syndicales et professionnelles ?":[3],
+        "QUXVlc3Rpb246MTA5 - Si oui, à quel type d'associations ou d'organisations ? Et avec quel rôle ?":[2],
+        "QUXVlc3Rpb246MTEw - Que faudrait-il faire pour renouer le lien entre les citoyens et les élus qui les représentent ?":[2],
+        "QUXVlc3Rpb246MTEx - Le non-cumul des mandats instauré en 2017 pour les parlementaires (députés et sénateurs) est :":[3],
+        "QUXVlc3Rpb246MTEy - Pourquoi ?":[1],
+        "QUXVlc3Rpb246MTEz - Que faudrait-il faire pour mieux représenter les différentes sensibilités politiques ?":[2],
+        "QUXVlc3Rpb246MTE1 - Si oui, lesquels ?":[3],
+        "QUXVlc3Rpb246MTE2 - Que pensez-vous de la participation des citoyens aux élections et comment les inciter à y participer davantage ?":[1],
+        "QUXVlc3Rpb246MTE4 - Si oui, de quelle manière ?":[2],
+        "QUXVlc3Rpb246MTE5 - Que faudrait-il faire aujourd'hui pour mieux associer les citoyens aux grandes orientations et à la décision publique ? Comment mettre en place une démocratie plus participative ?":[2],
+        "QUXVlc3Rpb246MTIx - Si oui, comment ?":[2],
+        "QUXVlc3Rpb246MTIy - Que faudrait-il faire pour consulter plus directement les citoyens sur l'utilisation de l'argent public, par l'Etat et les collectivités ?":[2],
+        "QUXVlc3Rpb246MTIz - Quel rôle nos assemblées, dont le Sénat et le Conseil économique, social et environnemental, doivent-elles jouer pour représenter nos territoires et la société civile ?":[3],
+        "QUXVlc3Rpb246MTI1 - Si oui, comment ?":[2],
+        "QUXVlc3Rpb246MTI3 - Que proposez-vous pour renforcer les principes de la laïcité dans le rapport entre l'Etat et les religions de notre pays ?":[2],
+        "QUXVlc3Rpb246MTI4 - Comment garantir le respect par tous de la compréhension réciproque et des valeurs intangibles de la République ?":[2],
+        "QUXVlc3Rpb246MTI5 - Que faudrait-il faire aujourd'hui pour renforcer l'engagement citoyen dans la société ?":[2],
+        "QUXVlc3Rpb246MTMw - Quels sont les comportements civiques qu'il faut promouvoir dans notre vie quotidienne ou collective ?":[3],
+        "QUXVlc3Rpb246MTMx - Que faudrait-il faire pour favoriser le développement de ces comportements civiques et par quels engagements concrets chacun peut-il y participer ?":[2],
+        "QUXVlc3Rpb246MTMy - Que faudrait-il faire pour valoriser l'engagement citoyen dans les parcours de vie, dans les relations avec l'administration et les pouvoirs publics ?":[2],
+        "QUXVlc3Rpb246MTMz - Quelles sont les incivilités les plus pénibles dans la vie quotidienne et que faudrait-il faire pour lutter contre ces incivilités ?":[3],
+        "QUXVlc3Rpb246MTM0 - Que peuvent et doivent faire les pouvoirs publics pour répondre aux incivilités ?":[2],
+        "QUXVlc3Rpb246MTM1 - Quel pourrait être le rôle de chacun pour faire reculer les incivilités dans la société ?":[2],
+        "QUXVlc3Rpb246MTM2 - Quelles sont les discriminations les plus répandues dont vous êtes témoin ou victime ?":[1],
+        "QUXVlc3Rpb246MTM3 - Que faudrait-il faire pour lutter contre ces discriminations et construire une société plus solidaire et plus tolérante ?":[2],
+        "QUXVlc3Rpb246MTM5 - Si oui, lesquelles ?":[3],
+        "QUXVlc3Rpb246MTQx - Que pensez-vous de la situation de l'immigration en France aujourd'hui et de la politique migratoire ? Quelles sont, selon vous, les critères à mettre en place pour définir la politique migratoire ?":[1,2],
+        "QUXVlc3Rpb246MTQz - Que proposez-vous afin de répondre à ce défi qui va durer ?":[2],
+        "QUXVlc3Rpb246MTQ0 - Quelles sont, selon vous, les modalités d'intégration les plus efficaces et les plus justes à mettre en place aujourd'hui dans la société ?":[3],
+        "QUXVlc3Rpb246MTQ1 - Y a-t-il d'autres points sur la démocratie et la citoyenneté sur lesquels vous souhaiteriez vous exprimer ?":[1,2],
+    },
+    "org_etat_et_services_publics":{
+        "QUXVlc3Rpb246MTY5 - Que pensez-vous de l'organisation de l'Etat et des administrations en France ? De quelle manière cette organisation devrait-elle évoluer ?":[1,2],
+        "QUXVlc3Rpb246MTcx - Si oui, lesquelles ?":[3],
+        "QUXVlc3Rpb246MTcy - Si non, quels types de services publics vous manquent dans votre territoire et qu'il est nécessaire de renforcer ?":[1,3],
+        "QUXVlc3Rpb246MTc0 - Quels nouveaux services ou quelles démarches souhaitez-vous voir développées sur Internet en priorité ?":[3],
+        "QUXVlc3Rpb246MTc3 - Quelles améliorations préconiseriez-vous ?":[2],
+        "QUXVlc3Rpb246MTc4 - Quand vous pensez à l'évolution des services publics au cours des dernières années, quels sont ceux qui ont évolué de manière positive ?":[1],
+        "QUXVlc3Rpb246MTc5 - Quels sont les services publics qui doivent le plus évoluer selon vous ?":[3],
+        "QUXVlc3Rpb246MTgy - Si oui, à quelle occasion en avez-vous fait usage ?":[3],
+        "QUXVlc3Rpb246MTgz - Pouvez-vous identifier des règles que l'administration vous a déjà demandé d'appliquer et que vous avez jugées inutiles ou trop complexes ?":[3],
+        "QUXVlc3Rpb246MTg1 - Si oui, comment ?":[2],
+        "QUXVlc3Rpb246MTg3 - Si oui, comment ?":[2],
+        "QUXVlc3Rpb246MTg4 - Comment l'Etat et les collectivités territoriales peuvent-ils s'améliorer pour mieux répondre aux défis de nos territoires les plus en difficulté ?":[2],
+        "QUXVlc3Rpb246MTkx - Si vous avez été amené à chercher une formation, pouvez-vous indiquer les éléments de "
+        "satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MTky - Si vous avez été amené à scolariser votre enfant, pouvez-vous indiquer les éléments de "
+        "satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MTkz - Si vous avez été amené à chercher un emploi, pouvez-vous indiquer les éléments de "
+        "satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MTk0 - Si vous avez été amené à préparer votre retraite, pouvez-vous indiquer les éléments de "
+        "satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MTk1 - Si vous avez été amené à demander un remboursement de soins de santé, pouvez-vous "
+        "indiquer les éléments de satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, "
+        "l'administration concernée :":[1,4],
+        "QUXVlc3Rpb246MTk2 - Si vous avez été amené à faire une demande d'aide pour une situation de handicap, "
+        "pouvez-vous indiquer les éléments de satisfaction et/ou les difficultés rencontrés en précisant, pour chaque "
+        "point, l'administration concernée :":[1,4],
+        "QUXVlc3Rpb246MTk4 - Si vous avez été amené à créer une entreprise, pouvez-vous indiquer les éléments de "
+        "satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MTk5 - Si vous avez été amené à recruter du personnel, pouvez-vous indiquer les éléments de "
+        "satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MjAw - Si vous avez été amené à former du personnel, pouvez-vous indiquer les éléments de "
+        "satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MjAx - Si vous avez été amené à rémunérer du personnel, pouvez-vous indiquer les éléments de "
+        "satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MjAy - Si vous avez été amené à mettre fin à votre activité, pouvez-vous indiquer les éléments "
+        "de satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, l'administration concernée "
+        ":":[1,4],
+        "QUXVlc3Rpb246MjAz - Si vous avez été amené à recruter une personne portant un handicap, pouvez-vous indiquer "
+        "les éléments de satisfaction et/ou les difficultés rencontrés en précisant, pour chaque point, "
+        "l'administration concernée :":[1,4],
+        "QUXVlc3Rpb246MTg5 - Y a-t-il d'autres points sur l'organisation de l'Etat et des services publics sur "
+        "lesquels vous souhaiteriez vous exprimer ?":[1,2],
+        }
+}
